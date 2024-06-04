481c22d5bb5e2d9cba0cb7ee69f5b1f2b4de7b9a
Jay <jay.jrjren@gmail.com>
2024-06-03 22:30:48 -0400

    Add icicle ability to mage

--- actors/allies/mage.gd
+++ actors/allies/mage.gd
@@ -3,4 +3,5 @@ class_name Mage extends Ally
 func _enter_tree():
 	add_ability(MoveAbility.new("Move", 1))
 	add_ability(DamageAbility.new("Fireball", 3, 1))
+	add_ability(DamageAbility.new("Icicle", 4, 5))
 

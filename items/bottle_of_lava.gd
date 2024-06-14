ea3086453940550a054de43624b2aadb269cbf73
Jay <jay.jrjren@gmail.com>
2024-06-13 20:16:16 -0400

    Update bottle_of_lava.gd

--- items/bottle_of_lava.gd
+++ items/bottle_of_lava.gd
@@ -1,10 +1,10 @@
 class_name BottleOfLava extends Item
 
 func modify_event_trigger(event_trigger: EventTrigger):
-	if not event_trigger.source == 
+	if not event_trigger.source() == owner: return
 	if not event_trigger.event is DamageEvent: return
 	
-	if event_trigger.source.rand() < 0.15:
+	if event_trigger.source().rand() < 0.15:
 		# TODO: Maybe instead of modifying the DamageEvent, create an
 		# ApplyDebuffEvent instead
 		event_trigger.event.burn = true

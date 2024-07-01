555a4a1432e2c0974f340fa4957a9016bbc48c7e
Jay <jay.jrjren@gmail.com>
2024-06-30 22:32:42 -0400

    Delete DISARMED status

--- status_effects/status_effect.gd
+++ status_effects/status_effect.gd
@@ -1,29 +1,32 @@
 class_name StatusEffect extends Resource
 
 enum Type {
+	# Take damage at start of turn
 	BLEED,
+	# Take damage at start of turn
 	BURN,
-	DISARMED,
-	FROZEN,
-	MUDDLED,
+	# Take damage at start of turn
 	POISONED,
+	# Skip turn, dies when health < 30%
+	FROZEN,
+	# Attacks deal 50% damage, rounded down
+	WEAKENED,
+	# Skip turn
 	STUNNED,
 }
 
 const duration_of: PackedInt32Array = [
 	2,
 	2,
-	1,
-	1,
-	1,
 	2,
+	1,
+	1,
 	1,
 ]
 
 const is_stacking: PackedByteArray = [
 	1,
 	1,
-	0,
 	0,
 	0,
 	0,
